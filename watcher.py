e4f891f517649e80ec8ef63d6ca5998cc9da0cde
neeryks <akashdeep.singh49@gmail.com>
2022-02-08 19:03:30 +0530

    Bug Fix

--- watcher.py
+++ watcher.py
@@ -1,5 +1,6 @@
 from email import header
 from os import sep
+from unittest import skip
 import pandas as pd
 from pyparsing import col
 from matplotlib import pyplot as pyp
@@ -9,11 +10,11 @@ from sklearn.model_selection import learning_curve
 import sklearn
 
 ### Starting ###
-
 dframe= pd.read_csv('data/owid-covid-data.csv')
 ldata = pd.read_csv('https://github.com/owid/covid-19-data/blob/master/public/data/latest/owid-covid-latest.csv')
 inddf = dframe[dframe['iso_code']=='IND']
 ldata = ldata[ldata['iso_code']=='IND']
+print(ldata,inddf)
 if inddf['Date'].iloc[-1] == ldata['Date']:
     pass
 elif inddf['Date'].iloc[-1] != ldata['Date']:
