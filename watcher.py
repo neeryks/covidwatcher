973d3c0b09c9f78ee26106f43b88aaa3eca0b181
neeryks <akashdeep.singh49@gmail.com>
2022-02-10 10:35:09 +0530

    Fixed Major Bug - Raw CSV import

--- watcher.py
+++ watcher.py
@@ -1,6 +1,7 @@
 from email import header
 from os import sep
 from unittest import skip
+from urllib import request
 import pandas as pd
 from pyparsing import col
 from matplotlib import pyplot as pyp
@@ -8,18 +9,10 @@ import seaborn as sns
 import plotly as pl
 from sklearn.model_selection import learning_curve
 import sklearn
+import http
 
 ### Starting ###
 dframe= pd.read_csv('data/owid-covid-data.csv')
-ldata = pd.read_csv('https://github.com/owid/covid-19-data/blob/master/public/data/latest/owid-covid-latest.csv')
-inddf = dframe[dframe['iso_code']=='IND']
-ldata = ldata[ldata['iso_code']=='IND']
-print(ldata,inddf)
-if inddf['Date'].iloc[-1] == ldata['Date']:
-    pass
-elif inddf['Date'].iloc[-1] != ldata['Date']:
-    dframe = pd.concat(inddf,ldata)
-    dframe.to_csv('data/owid-covid-data.csv')
-
-
-print(dframe)
+ldata = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv')
+# ldata = pd.save('https://github.com/owid/covid-19-data/blob/master/public/data/latest/owid-covid-latest.csv',on_bad_lines='skip')
+print(ldata)
