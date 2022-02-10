1a3a6f18adf44208b92c28b4afc849bffbf717cf
neeryks <akashdeep.singh49@gmail.com>
2022-02-10 19:09:49 +0530

    Added Relation between reproduction rate and deaths

--- watcher.py
+++ watcher.py
@@ -1,7 +1,5 @@
-from base64 import encode
-from os import sep
 import pandas as pd
-from matplotlib import pyplot as pyp
+from matplotlib import pyplot as plt
 import seaborn as sns
 import plotly as pl
 from sklearn.model_selection import learning_curve
@@ -10,8 +8,6 @@ import sklearn
 ### Starting ###
 dframe= pd.read_csv('data/data.csv',index_col=False)
 lframe = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv',index_col=False)
-colu = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-codebook.csv')
-colu = colu['column'].values.tolist()
 inddframe = dframe[dframe['iso_code']=='IND']
 indlframe = lframe[lframe['iso_code']=='IND']
 indlframe = indlframe.rename({'last_updated_date':'date'},axis=1)
@@ -19,8 +15,11 @@ indlframe = indlframe.rename({'last_updated_date':'date'},axis=1)
 
 if str(inddframe['date'].iloc[-1]) != str(indlframe['date'].iloc[0]) :
     inddframe = pd.concat([inddframe,indlframe])
-    print(inddframe)
+    inddframe = inddframe[['iso_code','location','date','total_cases','new_cases','total_deaths','new_deaths','reproduction_rate']].dropna(axis=0)
+    print(inddframe.head(20))
     inddframe.to_csv('data/data.csv',index=False)
 
-    
+sns.relplot(data=inddframe,x=inddframe['date'],y=inddframe['new_deaths'],sizes=(15,200),size='reproduction_rate')
+plt.show()
 
+    
