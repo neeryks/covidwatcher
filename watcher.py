930fbe9507f036fc68a1cecc1e969877f198f427
neeryks <akashdeep.singh49@gmail.com>
2022-02-11 16:47:06 +0530

    Added Vaccination Data

--- watcher.py
+++ watcher.py
@@ -1,3 +1,4 @@
+from numpy import cov
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -6,20 +7,42 @@ from sklearn.model_selection import learning_curve
 import sklearn
 
 ### Starting ###
-dframe= pd.read_csv('data/data.csv',index_col=False)
-lframe = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv',index_col=False)
-inddframe = dframe[dframe['iso_code']=='IND']
-indlframe = lframe[lframe['iso_code']=='IND']
-indlframe = indlframe.rename({'last_updated_date':'date'},axis=1)
+class dataframes():
+    def covidstat():
+        dframe= pd.read_csv('data/data.csv',index_col=False)
+        lframe = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv',index_col=False)
+        inddframe = dframe[dframe['iso_code']=='IND']
+        indlframe = lframe[lframe['iso_code']=='IND']
+        indlframe = indlframe.rename({'last_updated_date':'date'},axis=1)
+        print(inddframe.head())
 
 
-if str(inddframe['date'].iloc[-1]) != str(indlframe['date'].iloc[0]) :
-    inddframe = pd.concat([inddframe,indlframe])
-    inddframe = inddframe[['iso_code','location','date','total_cases','new_cases','total_deaths','new_deaths','reproduction_rate']].dropna(axis=0)
-    print(inddframe.head(20))
-    inddframe.to_csv('data/data.csv',index=False)
+        if str(inddframe['date'].iloc[-1]) != str(indlframe['date'].iloc[0]) :
+            inddframe = pd.concat([inddframe,indlframe])
+            inddframe = inddframe[['iso_code','location','date','total_cases','new_cases','total_deaths','new_deaths','reproduction_rate']].dropna(axis=0)
+            print(inddframe.head(20))
+            inddframe.to_csv('data/data.csv',index=False)
 
-sns.relplot(data=inddframe,x=inddframe['date'],y=inddframe['new_deaths'],sizes=(15,200),size='reproduction_rate')
+        return(inddframe)
+    
+    def vaccstat():
+        vaccdata = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/India.csv',index_col=False)
+        vaccdata = vaccdata[['date','people_vaccinated']]
+        vaccdata = vaccdata[1::].dropna(axis= 0)
+        print(vaccdata.head())
+
+        return(vaccdata)
+
+    new = pd.concat([covidstat(),vaccstat()],ignore_index=True)
+    new = new[['date','total_cases','people_vaccinated']].fillna(0)
+    new.to_csv('data/new.csv')
+
+sns.relplot(x='date',y='people_vaccinated',data = dataframes.new,kind='line')
 plt.show()
+
+
+
+
+
 
     
