ad3223cb8a2f38097b310ef7ceafbdcc02878fc6
neeryks <akashdeep.singh49@gmail.com>
2022-02-11 23:41:12 +0530

    Added Plotly & Plotting Graphs

--- watcher.py
+++ watcher.py
@@ -2,7 +2,7 @@ from numpy import cov
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
-import plotly as pl
+import plotly.express as px
 from sklearn.model_selection import learning_curve
 import sklearn
 
@@ -14,13 +14,13 @@ class dataframes():
         inddframe = dframe[dframe['iso_code']=='IND']
         indlframe = lframe[lframe['iso_code']=='IND']
         indlframe = indlframe.rename({'last_updated_date':'date'},axis=1)
-        print(inddframe.head())
+        #print(inddframe.head())
 
 
         if str(inddframe['date'].iloc[-1]) != str(indlframe['date'].iloc[0]) :
             inddframe = pd.concat([inddframe,indlframe])
             inddframe = inddframe[['iso_code','location','date','total_cases','new_cases','total_deaths','new_deaths','reproduction_rate']].dropna(axis=0)
-            print(inddframe.head(20))
+            #print(inddframe.head(20))
             inddframe.to_csv('data/data.csv',index=False)
 
         return(inddframe)
@@ -29,16 +29,19 @@ class dataframes():
         vaccdata = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/India.csv',index_col=False)
         vaccdata = vaccdata[['date','people_vaccinated']]
         vaccdata = vaccdata[1::].dropna(axis= 0)
-        print(vaccdata.head())
+        #print(vaccdata.head())
 
         return(vaccdata)
 
-    new = pd.concat([covidstat(),vaccstat()],ignore_index=True)
-    new = new[['date','total_cases','people_vaccinated']].fillna(0)
-    new.to_csv('data/new.csv')
+    
 
-sns.relplot(x='date',y='people_vaccinated',data = dataframes.new,kind='line')
-plt.show()
+    new = pd.merge(covidstat(),vaccstat(),on='date',how='left')
+    new = new[['date','total_cases','people_vaccinated']]
+    new.to_csv('data/new.csv')
+    print(new)
+
+fig = px.line(dataframes.new,x='date',y=dataframes.new.columns[1:2],title='Covid Cases with the span of time')
+fig.show()
 
 
 
