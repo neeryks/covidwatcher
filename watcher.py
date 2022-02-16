95734f50091d8383c3c2e5c2b4eaa6b656f64d16
neeryks <akashdeep.singh49@gmail.com>
2022-02-16 12:31:03 +0530

    Updated Watcher.py & Bug Fixes

--- watcher.py
+++ watcher.py
@@ -6,9 +6,9 @@ import pandas as pd
 import plotly.express as px
 from plotly import graph_objects as go
 from plotly.subplots import make_subplots as ms
-from sklearn.metrics import homogeneity_completeness_v_measure
-import json
+import ujson as json
 from statesorter import flsorter
+from multiprocessing import process
 
 
 ### Starting ###
@@ -63,8 +63,8 @@ geojson = json.load(open('data/INDIA_STATES.json','r'))
 #fig1 = px.choropleth(full,geojson='data/INDIA_STATES.json',color_continuous_scale="Viridis",featureidkey='properties.state_code',locations=full['state_code'],color=full['Active'],range_color=(0,700000),animation_frame=full['Last_Update'],title='Covid-19 Data [ INDIA ]')
 fig1 = px.choropleth_mapbox(full, geojson=geojson,
                       locations='state_code', 
-                      color='Active',
-                      color_continuous_scale="Viridis",
+                      color='New Deaths',
+                      color_continuous_scale="thermal",
                       range_color=(0,70000),
                       featureidkey="properties.state_code", 
                       animation_frame='Last_Update',
@@ -72,12 +72,14 @@ fig1 = px.choropleth_mapbox(full, geojson=geojson,
                       hover_data=["Confirmed","Active","Recovered","Deaths","Last_Update"],
                       hover_name="Province_State",
                       center={"lat": 23, "lon": 88},
-                      zoom=3.45,
-                      opacity=1,)
+                      opacity=1,
+                      zoom=3.45)
 
-fig1.update_layout(height=700, width=950, font=dict(family="Arial, Helvetica, sans-serif",size=24,), title_x=0.5, coloraxis_colorbar=dict(title="Covid cases"),)
-fig1.update_layout(coloraxis_colorbar_x=-0.25,)
+fig1.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 60
+fig1.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 5
+
 fig1.update_geos(fitbounds="locations", visible=False)
 fig1.show()
+
 
     
