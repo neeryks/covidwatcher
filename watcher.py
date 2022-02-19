a8a8ab93b023f312ac352c98f34c7115d9d9a229
neeryks <akashdeep.singh49@gmail.com>
2022-02-19 20:58:38 +0530

    Bug Fix

--- watcher.py
+++ watcher.py
@@ -9,7 +9,7 @@ from plotly.subplots import make_subplots as ms
 import ujson as json
 from statesorter import flsorter
 from multiprocessing import process
-
+from raceplotly.plots import barplot
 
 ### Starting ###
 class dataframes():
@@ -43,43 +43,47 @@ class dataframes():
     new = pd.merge(covidstat(),vaccstat(),on='date',how='left')
     new.to_csv('data/new.csv')
     #print(new)
+def scatter():
+    fig = ms(rows=3,cols=2,start_cell='bottom-left',subplot_titles=("People Vaccinated agaist Covid19","Every day Covid19 Cases","Deaths Every day Due to Covid19"
+    ,"Total Deaths Covid19","Total Cases"))
+    fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['people_vaccinated'],),row=1,col=1)
+    fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['new_cases']),row=1,col=2)
+    fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['new_deaths']),row=2,col=1)
+    fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['total_deaths']),row=2,col=2)
+    fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['total_cases']),row=3,col=1)
 
-fig = ms(rows=3,cols=2,start_cell='bottom-left',subplot_titles=("People Vaccinated agaist Covid19","Every day Covid19 Cases","Deaths Every day Due to Covid19"
-,"Total Deaths Covid19","Total Cases"))
-fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['people_vaccinated'],),row=1,col=1)
-fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['new_cases']),row=1,col=2)
-fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['new_deaths']),row=2,col=1)
-fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['total_deaths']),row=2,col=2)
-fig.add_trace(go.Scatter(x=dataframes.new['date'],y=dataframes.new['total_cases']),row=3,col=1)
-
-fig.update_layout(title_text="Covid19 Related Data INDIA")
-fig.show()
+    fig.update_layout(title_text="Covid19 Related Data INDIA")
+    fig.show()
 
 
-flsorter()
+    flsorter()
 full = pd.read_csv("data/statedata/full.csv")
 geojson = json.load(open('data/INDIA_STATES.json','r'))
 
 #fig1 = px.choropleth(full,geojson='data/INDIA_STATES.json',color_continuous_scale="Viridis",featureidkey='properties.state_code',locations=full['state_code'],color=full['Active'],range_color=(0,700000),animation_frame=full['Last_Update'],title='Covid-19 Data [ INDIA ]')
-fig1 = px.choropleth_mapbox(full, geojson=geojson,
-                      locations='state_code', 
-                      color='New Deaths',
-                      color_continuous_scale="thermal",
-                      range_color=(0,70000),
-                      featureidkey="properties.state_code", 
-                      animation_frame='Last_Update',
-                      mapbox_style="carto-positron",
-                      hover_data=["Confirmed","Active","Recovered","Deaths","Last_Update"],
-                      hover_name="Province_State",
-                      center={"lat": 23, "lon": 88},
-                      opacity=1,
-                      zoom=3.45)
+def coro():    
+    fig1 = px.choropleth_mapbox(full, geojson=geojson,
+                        locations='state_code', 
+                        color='New Deaths',
+                        color_continuous_scale="thermal",
+                        range_color=(0,70000),
+                        featureidkey="properties.state_code", 
+                        animation_frame='Last_Update',
+                        mapbox_style="carto-positron",
+                        hover_data=["Confirmed","Active","Recovered","Deaths","Last_Update"],
+                        hover_name="Province_State",
+                        center={"lat": 23, "lon": 88},
+                        opacity=1,
+                        zoom=3.45)
 
-#fig1.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 60
-#fig1.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 5
+    #fig1.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 60
+    #fig1.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 5
 
-fig1.update_geos(fitbounds="locations", visible=False)
-fig1.show()
+    fig1.update_geos(fitbounds="locations", visible=False)
+    fig1.show()
+def bp():
+    rp = barplot(full,item_column=full['Province_State'],value_column=full['Confirmed'],time_column=full['Last_Update'])
+    rp.plot(item_label="Confirmed Cases Covid-19 India",value_label="Population",frame_duration=600)
 
-
+bp()
     
